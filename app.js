d1069f0101e5707d23f1bfd6e004357e2a9fc401
ahmerkhn <155139134+ahmerkhn@users.noreply.github.com>
2024-05-25 05:46:04 -0700

    complete assignment

---
+++ app.js
@@ -0,0 +1,349 @@
+var userData = [
+    {
+        name: "Abdullah",
+        fatherName: "Mushtaq",
+        age: 37,
+        rollNo: 4654,
+        status: "present",
+        profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Bilal",
+        fatherName: "abc",
+        age: 34,
+        rollNo: 4565,
+        status: "leave",
+        profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Junaid",
+        fatherName: "xyz",
+        age: 87,
+        rollNo: 768,
+        status: "absent",
+        profilePic: "https://images.unsplash.com/photo-1711797750105-650ed71f5771?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+    name: 'John Doe',
+    fatherName: 'Richard Doe',
+    age: 16,
+    rollNo: '101',
+    class: '10th Grade',
+    status: "leave",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Jane Smith',
+    fatherName: 'Robert Smith',
+    age: 15,
+    rollNo: '102',
+    class: '9th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Michael Johnson',
+    fatherName: 'James Johnson',
+    age: 17,
+    rollNo: '103',
+    class: '11th Grade',
+    status: "present",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+  },
+  {
+    name: 'Emily Davis',
+    fatherName: 'William Davis',
+    age: 14,
+    rollNo: '104',
+    class: '8th Grade',
+    status: "present",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Jessica Brown',
+    fatherName: 'Charles Brown',
+    age: 16,
+    rollNo: '105',
+    class: '10th Grade',
+    status: "leave",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Daniel Wilson',
+    fatherName: 'Thomas Wilson',
+    age: 15,
+    rollNo: '106',
+    class: '9th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Sarah Taylor',
+    fatherName: 'Andrew Taylor',
+    age: 17,
+    rollNo: '107',
+    class: '11th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1711797750105-650ed71f5771?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'David Lee',
+    fatherName: 'George Lee',
+    age: 14,
+    rollNo: '108',
+    class: '8th Grade',
+    status: "present",
+    profilePic: "https://images.unsplash.com/photo-1711797750105-650ed71f5771?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Laura Martin',
+    fatherName: 'Paul Martin',
+    age: 16,
+    rollNo: '109',
+    class: '10th Grade',
+    status: "leave",
+    profilePic: "https://images.unsplash.com/photo-1711797750105-650ed71f5771?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Kevin White',
+    fatherName: 'Mark White',
+    age: 15,
+    rollNo: '110',
+    class: '9th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1711797750105-650ed71f5771?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Anna Harris',
+    fatherName: 'Steven Harris',
+    age: 17,
+    rollNo: '111',
+    class: '11th Grade',
+    status: "present", 
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Brian Clark',
+    fatherName: 'Edward Clark',
+    age: 14,
+    rollNo: '112',
+    class: '8th Grade',
+    status: "present",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Sophia Lewis',
+    fatherName: 'Henry Lewis',
+    age: 16,
+    rollNo: '113',
+    class: '10th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Christopher Walker',
+    fatherName: 'Frank Walker',
+    age: 15,
+    rollNo: '114',
+    class: '9th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Grace Hall',
+    fatherName: 'Peter Hall',
+    age: 17,
+    rollNo: '115',
+    class: '11th Grade',
+    status: "absent",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Justin Allen',
+    fatherName: 'Patrick Allen',
+    age: 14,
+    rollNo: '116',
+    class: '8th Grade',
+    status: "leave",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Olivia Young',
+    fatherName: 'Bruce Young',
+    age: 16,
+    rollNo: '117',
+    class: '10th Grade',  
+    status: "present",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Nathan Hernandez',
+    fatherName: 'Larry Hernandez',
+    age: 15,
+    rollNo: '118',
+    class: '9th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Mia King',
+    fatherName: 'Joe King',
+    age: 17,
+    rollNo: '119',
+    class: '11th Grade',
+    status: "absent",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Ethan Wright',
+    fatherName: 'Stanley Wright',
+    age: 14,
+    rollNo: '120',
+    class: '8th Grade',
+    status: "present",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Ava Lopez',
+    fatherName: 'Timothy Lopez',
+    age: 16,
+    rollNo: '121',
+    class: '10th Grade',
+    status: "leave",
+    profilePic: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1715937527911-14a019e0bd95?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Jacob Scott',
+    fatherName: 'Chris Scott',
+    age: 15,
+    rollNo: '122',
+    class: '9th Grade',
+    status: "leave",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Samantha Green',
+    fatherName: 'Shawn Green',
+    age: 17,
+    rollNo: '123',
+    class: '11th Grade',
+    status: "present",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Andrew Adams',
+    fatherName: 'Jason Adams',
+    age: 14,
+    rollNo: '124',
+    class: '8th Grade',
+    status: "present",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+  {
+    name: 'Ella Baker',
+    fatherName: 'Ryan Baker',
+    age: 16,
+    rollNo: '125',
+    class: '10th Grade',
+    status: "present",
+    profilePic: "https://plus.unsplash.com/premium_photo-1667861383621-4ebdc0a93a5e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+
+  },
+];
+
+var userTable = document.getElementById("userTable").getElementsByTagName('tbody')[0];
+var userName = document.getElementById("name");
+var fatherName = document.getElementById("fatherName");
+var rollno = document.getElementById("rollno");
+var age = document.getElementById("age");
+var profilePic = document.getElementById("profilePic");
+var options = document.getElementById("options");
+
+function addData() {
+    userTable.innerHTML = "";
+    for (var i = 0; i < userData.length; i++) {
+        userTable.innerHTML += `
+<tr>
+<td>${i + 1}</td>
+<td><img src="${userData[i].profilePic}" class="profile-pic" alt="Profile Picture"></td>
+<td>${userData[i].name}</td>
+<td>${userData[i].fatherName}</td>
+<td>${userData[i].rollNo}</td>
+<td>${userData[i].status}</td>
+<td class="heart-icon" onclick="likeUser(this)"><i class="fa-regular fa-heart"></i></td>
+</tr>
+`;
+    }
+}
+addData();
+
+function addUser() {
+    var obj = {
+        name: userName.value,
+        fatherName: fatherName.value,
+        rollNo: rollno.value,
+        age: age.value,
+        status: options.value,
+        profilePic: profilePic.value // Add profile picture URL
+    };
+
+    userData.push(obj);
+    addData();
+}
+
+function likeUser(element) {
+    if (element.firstElementChild.className === "fa-regular fa-heart") {
+        element.innerHTML = '<i class="fa-solid fa-heart"></i>';
+    } else {
+        element.innerHTML = '<i class="fa-regular fa-heart"></i>';
+    }
+}
